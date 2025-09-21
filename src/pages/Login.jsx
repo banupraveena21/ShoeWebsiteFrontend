5df06f7efbb0184c78532f830a2d97c51bd1213f
Banu Praveena21 <banupraveena21@gmail.com>
2025-09-21 22:42:04 +0530

    Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,7 +11,7 @@ export default function Login() {
   const handleSubmit = async(e)=>{
     e.preventDefault()
     try{
-      const res = await axios.post('http://127.0.0.1:8000/api/login/',{
+      const res = await axios.post('https://shoewebsitebackend.onrender.com/api/login/',{
         username: email,
         password: password
       })
